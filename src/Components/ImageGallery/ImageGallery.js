46be4cf432bc30decc1d444c0c3397ee7c4b8b47
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2023-11-06 00:50:06 +0600

    completed task

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import image1 from "../../images/image-1.webp";
 import image2 from "../../images/image-2.webp";
 import image3 from "../../images/image-3.webp";
@@ -65,129 +65,134 @@ const ImageGallery = () => {
     setItems(newItems);
   };
 
-
   const showGalleryText = selectedItems.length === 0;
 
   return (
-<div className="container mx-auto bg-base-100 rounded-lg shadow-2xl p-4 md:p-10 m-2 md:m-10 py-4 md:py-10 w-full">
-{showGalleryText && (
-  <h1 className="text-2xl font-bold mb-2 md:mb-4 text-start ml-2 md:ml-3 border-spacing-y-80">
-    Gallery
-  </h1>
-)}
-<div className="flex flex-col md:flex-row justify-between border-spacing-x-5 items-center mb-2 md:mb-4">
-  <div>
-    {selectedItems.length > 0 && (
-      <div className="flex items-center text-lg font-bold">
-        <FaCheckSquare
-          size={22}
-          className="text-blue-600 borrder mx-1 md:mx-3"
-        />
-        {selectedItems.length} Files Selected
-      </div>
-    )}
-  </div>
-  {selectedItems.length > 0 && (
-    <button onClick={onDelete} className="text-red-600 font-bold mt-2 md:mt-0">
-      Delete Files
-    </button>
-  )}
-</div>
-<div className="border-b-2 border-base-300 my-4"></div>
-
-  <SortableList onSortEnd={onSortEnd} draggedItemClassName="p-4 cursor-pointer border">
-
-  <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-2">
-  <div  className="grid lg:grid-cols-1 gap-2">
-
-    {items
-      .filter((_, index) => index === 0)
-      .map(({ id, image, selected }) => (
-        <SortableItem key={id}>
-          <div className={`m-2 cursor-pointer big-image md:ml-10`}>
-            <label className="image-container ">
-              <input
-                type="checkbox"
-                checked={selected}
-                onChange={() => toggleSelect(id)}
-                className={`checkbox ${selected ? "checkbox-hidden" : ""}`}
+    <div className="container mx-auto bg-base-100 rounded-lg shadow-2xl p-4 md:p-10 m-2 w-full">
+      {showGalleryText && (
+        <h1 className="text-2xl font-bold mb-2 md:mb-4 text-start ml-2 md:ml-3 border-spacing-y-80">
+          Gallery
+        </h1>
+      )}
+      <div className="flex flex-col md:flex-row justify-between border-spacing-x-5 items-center mb-2 md:mb-4">
+        <div>
+          {selectedItems.length > 0 && (
+            <div className="flex items-center text-lg font-bold">
+              <FaCheckSquare
+                size={22}
+                className="text-blue-600 borrder mx-1 md:mx-3"
               />
-              <img
-                className="pointer-events-none border rounded-lg"
-                src={image}
-                alt="images"
-              />
-            </label>
-          </div>
-        </SortableItem>
-      ))}
-    </div>
-    <div className="grid lg:grid-cols-3 gap-4">
-    {items
-      .filter((_, index) => index > 0 && index <= 6)
-      .map(({ id, image, selected }) => (
-        <SortableItem key={id}>
-          <div className={`relative m-2 cursor-pointer`}>
-            <label className="image-container">
-              <input
-                type="checkbox"
-                checked={selected}
-                onChange={() => toggleSelect(id)}
-                className={`checkbox ${selected ? "checkbox-hidden" : ""}`}
-              />
-              <img
-                className="w-52 h-52 pointer-events-none border rounded-lg"
-                src={image}
-                alt="images"
-              />
-            </label>
-          </div>
-        </SortableItem>
-     ) )}
-    </div>
-    </div>
-    <div className="grid lg:grid-cols-5 gap-3 lg:mr-5">
-    {items
-      .filter((_, index) => index > 0 && index > 6)
-      .map(({ id, image, selected }) => (
-        <SortableItem key={id}>
-          <div className={`relative m-2 cursor-pointer`}>
-            <label className="image-container">
-              <input
-                type="checkbox"
-                checked={selected}
-                onChange={() => toggleSelect(id)}
-                className={`checkbox ${selected ? "checkbox-hidden" : ""}`}
-              />
-              <img
-                className="w-52 h-52 pointer-events-none border rounded-lg"
-                src={image}
-                alt="images"
-              />
-            </label>
-          </div>
-        </SortableItem>
-     ) )}
-      <Dropzone onDrop={onDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div
-            className="text-center border border-dashed p-4 md:p-10 flex flex-col items-center justify-center"
-            {...getRootProps()}
+              {selectedItems.length} Files Selected
+            </div>
+          )}
+        </div>
+        {selectedItems.length > 0 && (
+          <button
+            onClick={onDelete}
+            className="text-red-600 hover:underline font-bold mt-2 md:mt-0"
           >
-            <input {...getInputProps()} />
-            <IoIosImages size={30} color="#000" />
-            <p>Add Images</p>
-          </div>
+            Delete Files
+          </button>
         )}
-      </Dropzone>
-    </div>
-  </SortableList>
-</div>
+      </div>
+      <div className="border-b-2 border-base-300 my-4"></div>
 
+      <SortableList
+        onSortEnd={onSortEnd}
+        draggedItemClassName="p-4 cursor-pointer border"
+      >
+        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-2">
+          <div className="grid lg:grid-cols-1 gap-2">
+            {items
+              .filter((_, index) => index === 0)
+              .map(({ id, image, selected }) => (
+                <SortableItem key={id}>
+                  <div className={`m-2 cursor-pointer big-image lg:ml-10`}>
+                    <label className="image-container ">
+                      <input
+                        type="checkbox"
+                        checked={selected}
+                        onChange={() => toggleSelect(id)}
+                        className={`checkbox ${
+                          selected ? "checkbox-hidden" : ""
+                        }`}
+                      />
+                      <img
+                        className="pointer-events-none border rounded-lg"
+                        src={image}
+                        alt="images"
+                      />
+                    </label>
+                  </div>
+                </SortableItem>
+              ))}
+          </div>
+          <div className="grid lg:grid-cols-3 gap-4">
+            {items
+              .filter((_, index) => index > 0 && index <= 6)
+              .map(({ id, image, selected }) => (
+                <SortableItem key={id}>
+                  <div className={`relative m-2 cursor-pointer`}>
+                    <label className="image-container">
+                      <input
+                        type="checkbox"
+                        checked={selected}
+                        onChange={() => toggleSelect(id)}
+                        className={`checkbox ${
+                          selected ? "checkbox-hidden" : ""
+                        }`}
+                      />
+                      <img
+                        className="w-52 h-52 pointer-events-none border rounded-lg"
+                        src={image}
+                        alt="images"
+                      />
+                    </label>
+                  </div>
+                </SortableItem>
+              ))}
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-5 gap-3">
+          {items
+            .filter((_, index) => index > 0 && index > 6)
+            .map(({ id, image, selected }) => (
+              <SortableItem key={id}>
+                <div className={`relative m-2 cursor-pointer`}>
+                  <label className="image-container">
+                    <input
+                      type="checkbox"
+                      checked={selected}
+                      onChange={() => toggleSelect(id)}
+                      className={`checkbox ${
+                        selected ? "checkbox-hidden" : ""
+                      }`}
+                    />
+                    <img
+                      className="w-52 h-52 pointer-events-none border rounded-lg"
+                      src={image}
+                      alt="images"
+                    />
+                  </label>
+                </div>
+              </SortableItem>
+            ))}
+          <Dropzone onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                className="text-center border border-dashed p-4 md:p-10 flex flex-col items-center justify-center"
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <IoIosImages size={30} color="#000" />
+                <p>Add Images</p>
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      </SortableList>
+    </div>
   );
 };
 
-
-
 export default ImageGallery;
-
